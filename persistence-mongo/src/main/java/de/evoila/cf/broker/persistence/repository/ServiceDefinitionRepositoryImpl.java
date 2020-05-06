ed0105fca0be1e847198266e7b095f74ee793b2b
René Schollmeyer <rschollmeyer@evoila.de>
2020-05-06 09:05:16 +0200

    add some logging

--- persistence-mongo/src/main/java/de/evoila/cf/broker/persistence/repository/ServiceDefinitionRepositoryImpl.java
+++ persistence-mongo/src/main/java/de/evoila/cf/broker/persistence/repository/ServiceDefinitionRepositoryImpl.java
@@ -5,6 +5,8 @@ import de.evoila.cf.broker.model.catalog.plan.Plan;
 import de.evoila.cf.broker.model.catalog.ServiceDefinition;
 import de.evoila.cf.broker.repository.ServiceDefinitionRepository;
 import de.evoila.cf.broker.service.CatalogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +16,8 @@ import java.util.List;
  */
 @Repository
 public class ServiceDefinitionRepositoryImpl implements ServiceDefinitionRepository {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
 
 	private CatalogService catalogService;
 
@@ -40,7 +44,9 @@ public class ServiceDefinitionRepositoryImpl implements ServiceDefinitionReposit
 	@Override
 	public Plan getPlan(String planId) throws ServiceDefinitionDoesNotExistException {
 		for(ServiceDefinition serviceDefinition : catalogService.getCatalog().getServices()) {
+			log.info("Service Definition: " + serviceDefinition.getId());
 			for (Plan currentPlan : serviceDefinition.getPlans()) {
+				log.info("Plan: " + currentPlan.getId());
 				if (currentPlan.getId().equals(planId)) {
 					return currentPlan;
 				}
